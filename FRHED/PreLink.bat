6c52207ce241d0a7e7e1a0ad1ebc123ea017de04
datadiode <datadiode@users.noreply.github.com>
2021-02-03 08:40:37 +0100

    PreLink.bat: Skip updating of .po files as it seems to cause CI builds to be flagged dirty

--- FRHED/PreLink.bat
+++ FRHED/PreLink.bat
@@ -2,10 +2,10 @@ cd
 echo %0
 echo $(IntDir) = %1
 echo $(TargetPath) = %2
-cd ..\Translations\Frhed
-cscript CreateMasterPotFile.vbs
-cscript UpdatePoFilesFromPotFile.vbs
-cd ..\..\FRHED
+REM ~ cd ..\Translations\Frhed
+REM ~ cscript CreateMasterPotFile.vbs
+REM ~ cscript UpdatePoFilesFromPotFile.vbs
+REM ~ cd ..\..\FRHED
 rc /fo%~1\lang.res /i.. ..\Translations\Frhed\heksedit.rc
 mkdir %~2\..\Languages
 link /DLL /NOENTRY /MACHINE:IX86 /OUT:%~2\..\Languages\heksedit.lng %~1\lang.res
